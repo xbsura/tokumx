39b635cc7985217880eed23ed8dc29f94266355c
Spencer T Brody <spencer@10gen.com>
2012-11-08 17:06:10 -0500

    Add AuthorizationManager to ClientBasic SERVER-7572
    Conflicts:
    	src/mongo/db/client.cpp
    	src/mongo/db/replutil.h

--- src/mongo/db/client.cpp
+++ src/mongo/db/client.cpp
@@ -21,20 +21,25 @@
    to an open socket (or logical connection if pooling on sockets) from a client.
 */
 
-#include "pch.h"
-#include "db.h"
-#include "client.h"
-#include "curop.h"
-#include "json.h"
-#include "security.h"
-#include "commands.h"
-#include "instance.h"
-#include "../s/d_logic.h"
-#include "dbwebserver.h"
-#include "../util/mongoutils/html.h"
-#include "../util/mongoutils/checksum.h"
-#include "repl/rs.h"
-#include "../scripting/engine.h"
+#include "mongo/pch.h"
+
+#include "mongo/db/client.h"
+
+#include "mongo/base/status.h"
+#include "mongo/db/auth/authorization_manager.h"
+#include "mongo/db/auth/auth_external_state_impl.h"
+#include "mongo/db/db.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/dbwebserver.h"
+#include "mongo/db/instance.h"
+#include "mongo/db/json.h"
+#include "mongo/db/repl/rs.h"
+#include "mongo/s/d_logic.h"
+#include "mongo/scripting/engine.h"
+#include "mongo/db/security.h"
+#include "mongo/util/mongoutils/checksum.h"
+#include "mongo/util/mongoutils/html.h"
+#include "mongo/util/mongoutils/str.h"
 
 namespace mongo {
   
@@ -51,6 +56,21 @@ namespace mongo {
         Client *c = new Client(desc, mp);
         currentClient.reset(c);
         mongo::lastError.initThread();
+        if (mp != NULL) {
+            // This thread corresponds to an incoming user connection, and thus needs an
+            // AuthorizationManager
+            AuthExternalStateImpl* externalState = new AuthExternalStateImpl();
+            AuthorizationManager* authManager = new AuthorizationManager(externalState);
+            // Go into God scope so that the AuthorizationManager can query the local admin DB
+            // as part of its initialization without needing auth.
+            GodScope gs;
+            Status status = authManager->initialize(new DBDirectClient());
+            massert(16480,
+                    mongoutils::str::stream() << "Error initializing AuthorizationManager: "
+                                              << status.reason(),
+                    status == Status::OK());
+            c->setAuthorizationManager(authManager);
+        }
         return *c;
     }
 
