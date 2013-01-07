1a3fcce4793d4879098cba56064787e734ba8cf6
Eliot Horowitz <eliot@10gen.com>
2013-01-07 10:58:53 -0500

    do not run map performance test in debug builds

--- src/mongo/util/string_map_test.cpp
+++ src/mongo/util/string_map_test.cpp
@@ -187,6 +187,7 @@ namespace {
         return t.micros();
     }
 
+#if !defined(_DEBUG)
     TEST( StringMapTest, perf1 ) {
         unsigned long long standard = 0;
         unsigned long long unordered = 0;
@@ -211,4 +212,5 @@ namespace {
         log() << "StringMap:\t" << custom << std::endl;
         ASSERT_LESS_THAN( custom, standard );
     }
+#endif
 }
