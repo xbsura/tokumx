945511e2520fd0d085d490a7dbb5481012d0db2b
Zardosht Kasheff <zardosht@gmail.com>
2013-05-28 16:35:12 -0400

    refs #112, patch up rollback

--- src/mongo/db/repl/bgsync.cpp
+++ src/mongo/db/repl/bgsync.cpp
@@ -485,6 +485,11 @@ namespace mongo {
                 BSONObj remoteObj = rollbackCursor->next();
                 GTID remoteGTID = getGTIDFromBSON("_id", remoteObj);
                 uint64_t remoteTS = remoteObj["ts"]._numberLong();
+                if (oplogTS == 0) {
+                    // in case we are rolling back due to not finding an element,
+                    // see isRollbackRequired
+                    oplogTS = remoteTS;
+                }
                 uint64_t remoteLastHash = remoteObj["h"].numberLong();
                 if (remoteTS + 1800*1000 < oplogTS) {
                     throw RollbackOplogException("replSet rollback too long a time period for a rollback (at least 30 minutes).");
@@ -617,7 +622,8 @@ namespace mongo {
             //  - remote oplog is empty for some weird reason
             // in either case, if it (strangely) happens, we'll just return
             // and our caller will simply try again after a short sleep.
-            log() << "replSet error empty query result from " << hn << " oplog" << rsLog;
+            log() << "replSet error empty query result from " << hn << " oplog, attempting rollback" << rsLog;
+            runRollback(r, ts);
             return true;
         }
 
